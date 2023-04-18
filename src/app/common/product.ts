90f87372a4cfdd9d0df6354cc8622ec9cbd55d78
VeeraEzhilVendhan <veeraezhilvendhan.v@gmail.com>
2023-04-18 19:20:15 +0530

    Initial Commit

---
+++ src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    sku: string="";
+    name: string="";
+    description: string="";
+    unitPrice: string="";
+    imageUrl: string="";
+    active: boolean| undefined;
+    unitsInStock: number| undefined;
+    dateCreated: Date| undefined;
+    lastUpdated: Date | undefined;
+}
